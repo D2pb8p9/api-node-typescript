c7e18b70705e1f0662b4a0769c97c53667ee7c5c
D2pb8p9 <danielpires2000@hotmail.com>
2024-12-06 10:21:55 -0300

    feat: initial config

---
+++ src/server/Server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const server = express();
+
+server.use(cors());
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send('Ola, DEV!')
+});
+
+export { server };
